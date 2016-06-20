f0f514d8317de625a6822fef4d57006e1f111b2d
ssliteris <ssliteris@deloitte.com>
2016-06-20 12:50:20 +0300

    enable cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 
+
+/*
+ * Enables CORS
+ */
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // parse application/json
 app.use(bodyParser.json())
 
